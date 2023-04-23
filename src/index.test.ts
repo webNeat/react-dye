9ba30d95632f4b5ee961c6c0ad5972e9db7191a1
Amine Ben hammou <abenhammou-ext@monoprixonline.fr>
2023-04-23 03:16:31 +0200

    Release 3.0.0-alpha.1

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import dye from '.'
+
+it('runs tests', () => {
+  expect(dye('custom-class')).toBeTruthy()
+})
--- src/internals/types.ts
+++
@@ -1,5 +0,0 @@
-export type StrMap<T = any> = {
-  [key: string]: T
-}
-
-export type ValueOf<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer E> ? E : never
